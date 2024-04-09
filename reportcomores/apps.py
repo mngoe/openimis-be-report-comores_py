a9f4dc4654267fdb9d877b45dee548fde843ebeb
kevel <keveltiah@gmail.com>
2024-04-09 15:04:10 +0100

    feat: Ajout module rapoort comores

---
+++ reportcomores/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReportcomoresConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reportcomores'
---
+++ reportcomores/report.py
@@ -0,0 +1,14 @@
+from reportcomores.report_templates import rptBeneficiaryAmg
+from reportcomores.models import generate_carte_amg_query
+
+report_definitions = [
+    {
+        "name": "carte_amg",
+        "engine": 0,
+        "default_report": rptBeneficiaryAmg.template,
+        "description": "Carte AMG",
+        "module": "insuree",
+        "python_query": generate_carte_amg_query,
+        "permission": ["131215"],
+    }
+]
