67de3ea5aeb70f292caa475c7e1dbd355b08c0f7
kevel choumene <k.choumene@cloudfacile.com>
2024-07-20 12:20:42 +0100

    debug

--- reportcomores/apps.py
+++ reportcomores/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ReportcomoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reportcomores'
+
+    def ready(self):
+        print("App is ready..................................")
--- reportcomores/report.py
+++ reportcomores/report.py
@@ -9,6 +9,6 @@ report_definitions = [
         "description": "Carte AMG",
         "module": "reportcomores",
         "python_query": generate_carte_amg_query,
-        "permission": ["131215"],
+        "permission": ["131217"],
     }
 ]
