dcf020cd9604617c037fc16878433440772816f7
kevel choumene <k.choumene@cloudfacile.com>
2025-02-02 21:32:07 +0100

    feat: Rapport adhésion

--- reportcomores/models.py
+++ reportcomores/models.py
@@ -8,6 +8,158 @@ from insuree.models import InsureePolicy
 import calendar
 from location.models import Location, HealthFacility
 from claim.models import Claim, ClaimService, ClaimItem
+import random
+from policy.models import Policy
+import imghdr, os
+from PIL import Image
+
+val_de_zero = [
+    'million', 'milliard', 'billion',
+    'quadrillion', 'quintillion', 'sextillion',
+    'septillion', 'octillion', 'nonillion',
+    'décillion', 'undecillion', 'duodecillion',
+    'tredecillion', 'quattuordecillion', 'sexdecillion',
+    'septendecillion', 'octodecillion', 'icosillion', 'vigintillion'
+]
+
+to_19_fr = (
+    'zéro', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six',
+    'sept', 'huit', 'neuf', 'dix', 'onze', 'douze', 'treize',
+    'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf'
+)
+
+tens_fr  = (
+    'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante',
+    'quatre-vingts', 'quatre-vingt'
+)
+
+denom_fr = (
+    '', 'mille', 'million', 'milliard', 'billion', 'quadrillion',
+    'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion',
+    'décillion', 'undecillion', 'duodecillion', 'tredecillion',
+    'quattuordecillion', 'sexdecillion', 'septendecillion',
+    'octodecillion', 'icosillion', 'vigintillion'
+)
+
+denoms_fr = (
+    '', 'mille', 'millions', 'milliards', 'billions', 'quadrillions',
+    'quintillions', 'sextillions', 'septillions', 'octillions', 'nonillions',
+    'décillions', 'undecillions', 'duodecillions', 'tredecillions',
+    'quattuordecillions', 'sexdecillions', 'septendecillions',
+    'octodecillions', 'icosillions', 'vigintillions'
+)
+
+def _convert_nnn_fr(val):
+    """
+    \detail         convert a value < 1000 to french
+        special cased because it is the level that kicks 
+        off the < 100 special case.  The rest are
+        more general.  This also allows you to
+        get strings in the form of 'forty-five hundred' if called directly.
+    \param  val     value(int or float) to convert
+    \return         a string value
+    """
+    word = ''
+    (mod, rem) = (val % 100, val // 100)
+    if rem > 0:
+        if (rem>1 and rem <10 and mod <= 0): 
+             word = to_19_fr[rem] + ' cents'
+        else: 
+             word = to_19_fr[rem] + ' cent'
+             
+        if mod > 0:
+            word += ' '
+    if mod > 0:
+        word += _convert_nn_fr(mod)
+    return word
+
+def _convert_nn_fr(val):
+    """
+    \brief       convert a value < 100 to French
+    \param  val  value to convert 
+    """
+    if val < 20:
+        return to_19_fr[val]
+    for (dcap, dval) in ((k, 20 + (10 * v)) for (v, k) in enumerate(tens_fr)):
+        if dval + 10 > val:
+            if dval in (70,90):
+                return dcap + '-' + to_19_fr[val % 10 + 10]
+            if val % 10:
+                return dcap + '-' + to_19_fr[val % 10]
+            return dcap
+
+def french_number(val):
+    
+    """
+    \brief       Convert a numeric value to a french string
+        Dispatch diffent kinds of number depending
+        on their value (<100 or < 1000)
+        Then create a for loop to rewrite cutted number.
+    \param  val  value(int or float) to convert
+    \return      a string value
+    """
+    
+    if val < 100:
+        return _convert_nn_fr(val)
+    if val < 1000:
+         return _convert_nnn_fr(val)
+    for (didx, dval) in ((v - 1, 1000 ** v) for v in range(len(denom_fr))):
+        if dval > val:
+            mod = 1000 ** didx
+            l = val // mod
+            r = val - (l * mod)
+            
+            pref_final = _convert_nnn_fr(l)
+            pref = pref_final.split(' ')
+            if(pref[len(pref)-1] == ' cent'):
+                pref[len(pref)-1] = " cents"
+                pref_final = " ".join(x for x in pref)
+            if l>1:    
+                ret = pref_final + ' ' + denoms_fr[didx]
+            else:
+                ret = pref_final + ' ' + denom_fr[didx]
+            if r > 0:
+                ret = ret + ' ' + french_number(r)
+            return ret
+
+def amount_to_text_fr(number, currency):
+    """
+    \brief              convert amount value to french string
+        reuse the french_number function
+        to write the correct number
+        in french, then add the specific cents for number < 0
+        and add the currency to the string
+    \param  number      the number to convert
+    \param  currency    string value of the currency
+    \return             the string amount in french
+    """
+    try:
+        number = int(number)
+    except:
+        return 'Traduction error'
+    number = '%.2f' % number
+    units_name = currency
+    list = str(number).split('.')
+    start_word = french_number(abs(int(list[0])))
+
+    #On enleve le un au debut de la somme écrite en lettre.
+    liste = str(start_word).split(' ')
+    for i in range(len(liste)):
+        item = liste[i]
+        tab=liste
+        if item =='un':
+            if i==0 and len(liste) > 1:
+                if liste[i+1] not in val_de_zero:
+                    tab[i]=""
+            elif i > 0 and len(liste) > 1:
+                if i < len(liste)-1:
+                    if liste[i+1] not in val_de_zero:
+                        if not liste[i-1] in ["cent", "cents"] and not (liste[i+1] in val_de_zero or liste[i+1] in denom_fr or liste[i+1] in denoms_fr):
+                            tab[i]=""
+            start_word = " ".join(x for x in tab)
+    french_number(int(list[1]))
+    final_result = start_word +' '+units_name
+    return final_result
 
 def generate_carte_amg_query(user, **kwargs):
     print("Carte AMG ", kwargs)
@@ -484,3 +636,245 @@ def invoice_public_fosa_query(user, **kwargs):
     final_data["data"] = invoice_data
     print(final_data)
     return final_data
+
+
+
+
+def report_membership_query(user, **kwargs):
+    print("Rapport adhesion ", kwargs)
+    ids = kwargs.get("familyID", [])
+    family_ids = []
+    if ids:
+        ids = ids.split(',')
+        family_ids = [eval(i) for i in ids]
+    family_list = Family.objects.filter(
+        id__in=family_ids,
+        validity_to__isnull=True
+    )
+    data = []
+    data2 = []
+    dictbase = {}
+    i = 0
+    print("family_list ", family_list)
+    if family_list:
+        for family in family_list:
+            dictbase["headDigit1"] = " "
+            dictbase["headDigit2"] = " "
+            dictbase["headDigit3"] = " "
+            dictbase["headDigit4"] = " "
+            dictbase["headDigit5"] = " "
+            dictbase["headDigit6"] = " "
+            dictbase["headDigit7"] = " "
+
+            dictbase["headDigit1"] = " "
+            dictbase["headDigit2"] = " "
+            dictbase["headDigit3"] = " "
+            dictbase["headDigit4"] = " "
+            dictbase["headDigit5"] = " "
+            dictbase["headDigit6"] = " "
+            dictbase["headDigit7"] = " "
+            dictbase["email"] = "..............................................................................................."
+            
+            head = family.head_insuree
+            if head.email:
+                dictbase["email"] = head.email
+            print("chec ", head.profession)
+            dictbase["familyType"] = "............................................"
+            dictbase["profession"] = "........................................"
+            if family.family_type:
+                dictbase["familyType"] = family.family_type.alt_language
+            if head.profession:
+                dictbase["profession"] = head.profession.alt_language
+            if family.location:
+                hflocation_obj = Location.objects.filter(
+                    id=family.location.id,
+                    validity_to__isnull=True
+                ).first()
+                if hflocation_obj:
+                    level_village = False
+                    level_district = False
+                    level_ville = False
+                    municipality = "...................................................................................."
+                    district = " "
+                    village = ".............................................."
+                    region = "...................................................................................."
+                    if hflocation_obj.parent:
+                        level_district = True
+                        if hflocation_obj.parent.parent:
+                            level_ville = True
+                            if hflocation_obj.parent.parent.parent:
+                                level_village = True
+                    if level_village:
+                        village = hflocation_obj.name
+                        municipality = hflocation_obj.parent.name
+                        district = hflocation_obj.parent.parent.code
+                        region = hflocation_obj.parent.parent.parent.code
+                    elif level_ville:
+                        municipality = hflocation_obj.name
+                        district = hflocation_obj.parent.code
+                        region = hflocation_obj.parent.parent.code
+                    elif level_district:
+                        district = hflocation_obj.code
+                        region = hflocation_obj.parent.code
+                    else:
+                        region = hflocation_obj.location.code
+                    dictbase["locality"] = village
+                    dictbase["region"] = region
+                    dictbase["community"] = municipality
+            if head.phone and len(head.phone) >= 7:
+                if family.parent:
+                    dictbase["ConjointDigit1"] = head.phone[0]
+                    dictbase["ConjointDigit2"] = head.phone[1]
+                    dictbase["ConjointDigit3"] = head.phone[2]
+                    dictbase["ConjointDigit4"] = head.phone[3]
+                    dictbase["ConjointDigit5"] = head.phone[4]
+                    dictbase["ConjointDigit6"] = head.phone[5]
+                    dictbase["ConjointDigit7"] = head.phone[6]
+                else:
+                    dictbase["headDigit1"] = head.phone[0]
+                    dictbase["headDigit2"] = head.phone[1]
+                    dictbase["headDigit3"] = head.phone[2]
+                    dictbase["headDigit4"] = head.phone[3]
+                    dictbase["headDigit5"] = head.phone[4]
+                    dictbase["headDigit6"] = head.phone[5]
+                    dictbase["headDigit7"] = head.phone[6]
+            members = Insuree.objects.filter(
+                family_id=family.id,
+                validity_to__isnull=True
+            ).exclude(id=head.id)
+            for membre in members:
+                if membre.relationship:
+                    values = {}
+                    if str(membre.relationship.relation).lower() in ["spouse", "époux"]:
+                        values["libelle"] = "Conjoint(e)"
+                        values["chfid"] = membre.chf_id
+                        values["FirstName"] = membre.last_name
+                        values["LastName"] = membre.other_names
+                        values["dob"] = str(membre.dob)
+                        if membre.gender:
+                            values["sex"] = str(membre.gender.code)
+                        if membre.phone and len(membre.phone) >= 7:
+                            if family.parent:
+                                dictbase["headDigit1"] = membre.phone[0]
+                                dictbase["headDigit2"] = membre.phone[1]
+                                dictbase["headDigit3"] = membre.phone[2]
+                                dictbase["headDigit4"] = membre.phone[3]
+                                dictbase["headDigit5"] = membre.phone[4]
+                                dictbase["headDigit6"] = membre.phone[5]
+                                dictbase["headDigit7"] = membre.phone[6]
+                            else:
+                                dictbase["ConjointDigit1"] = membre.phone[0]
+                                dictbase["ConjointDigit2"] = membre.phone[1]
+                                dictbase["ConjointDigit3"] = membre.phone[2]
+                                dictbase["ConjointDigit4"] = membre.phone[3]
+                                dictbase["ConjointDigit5"] = membre.phone[4]
+                                dictbase["ConjointDigit6"] = membre.phone[5]
+                                dictbase["ConjointDigit7"] = membre.phone[6]
+                        data.append(values)
+                    elif str(membre.relationship.relation).lower() in ["fils/fille", "son/daughter"]:
+                        values["libelle"] = "Bénéficiaire"
+                        values["chfid"] = membre.chf_id
+                        values["FirstName"] = membre.last_name
+                        values["LastName"] = membre.other_names
+                        values["dob"] = str(membre.dob)
+                        if membre.gender:
+                            values["sex"] = str(membre.gender.code)
+                        data.append(values)
+                    else:
+                        values2 = {}
+                        values2["libelle"] = "Bénéficiaire"
+                        values2["chfid"] = membre.chf_id
+                        values2["FirstName"] = membre.last_name
+                        values2["LastName"] = membre.other_names
+                        values2["dob"] = str(membre.dob)
+                        if membre.gender:
+                            values2["sex"] = str(membre.gender.code)
+                        data2.append(values2)
+            insure_policy = InsureePolicy.objects.filter(
+                insuree=head.id, validity_to__isnull=True
+            )
+            dictbase["total"] = ""
+            if insure_policy:
+                inspolicy  = insure_policy[0]
+                policy = Policy.objects.filter(id=inspolicy.policy.id).first()
+                if policy:
+                    dictbase["total"] = str("{:,.0f}".format(float(policy.value))) + " FC " + amount_to_text_fr(int(policy.value), 'FC')
+                    dictbase["jour"] = str(policy.start_date).split("-")[2]
+                    dictbase["mois"] = str(policy.start_date).split("-")[1]
+                    dictbase["annee"] = str(policy.start_date).split("-")[0]
+                else:
+                    dictbase["jour"] = "......."
+                    dictbase["mois"] = "......."
+                    dictbase["annee"] = "......."
+            dictbase["immat"] = head.chf_id
+            dictbase["firstName"] = head.last_name
+            dictbase["lastName"] = head.other_names
+            dictbase["address"] = head.current_address
+            if head.gender:
+                if head.gender.code == 'M':
+                    dictbase["civiliteM"] = "x"
+                    dictbase["civiliteF"] = ""
+                if head.gender.code == 'F':
+                    dictbase["civiliteM"] = "x"
+                    dictbase["civiliteM"] = ""
+    today = datetime.datetime.now()
+    today_date = str(today).split(" ")[0]
+    encoded_img = False
+    if head.photo and head.photo.photo:
+        imageData = str(head.photo.photo)
+        myimage = base64.b64decode((imageData))
+        extension = imghdr.what(None, h=myimage)
+        print("extension ", extension)
+        if str(extension).lower() != 'png':
+            # save image to png, image can have different format leading to an
+            # error : image is not PNG
+            imgFile = open('/tmp/'+head.chf_id+'.jpeg', 'wb')
+            imgFile.write(myimage)
+            imgFile.close()
+
+            img1 = Image.open(r'/tmp/'+head.chf_id+'.jpeg')
+            img1.save(r'/tmp/'+head.chf_id+'.png')
+
+            with open('/tmp/'+head.chf_id+'.png', "rb") as image_file:
+                encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+        else:
+            # already the expected extension (PNG)
+            encoded_img = imageData
+    else:
+        filename = ""
+        try:
+            filename = "openIMISphone/"+head.photo.folder+"/"+head.photo.filename
+        except Exception as e:
+            print(e)
+        print(filename)
+        if os.path.exists(filename):
+            with open(filename, "rb") as image_file:
+                imageData = base64.b64encode(image_file.read()).decode('utf-8')
+                myimage = base64.b64decode((imageData))
+                extension = imghdr.what(None, h=myimage)
+                print("extension is: ", extension)
+                if str(extension).lower() != 'png':
+                    # save image to png, image can have different format leading to an
+                    # error : image is not PNG
+                    imgFile = open('/tmp/'+head.chf_id+'.jpeg', 'wb')
+                    imgFile.write(myimage)
+                    imgFile.close()
+
+                    img1 = Image.open(r'/tmp/'+head.chf_id+'.jpeg')
+                    img1.save(r'/tmp/'+head.chf_id+'.png')
+
+                    with open('/tmp/'+head.chf_id+'.png', "rb") as image_file:
+                        encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+                else:
+                    # already the expected extension (PNG)
+                    encoded_img = imageData
+        # else:
+        #     with open("default-img.png", "rb") as image_file:
+        #         encoded_img = base64.b64encode(image_file.read()).decode('utf-8')
+        #     print("File not found")
+    dictbase['data'] = data
+    dictbase['data2'] = data2
+    if encoded_img:
+        dictbase["PhotoInsuree"] = "data:image/png;base64,"+str(encoded_img),
+    # print(dictbase)
+    return dictbase
