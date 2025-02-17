d1669b12389ebec751712bb088625afd329e4fe8
kevel choumene <k.choumene@cloudfacile.com>
2025-02-17 15:00:49 +0100

    feat: use of address instead of current_address

--- reportcomores/models.py
+++ reportcomores/models.py
@@ -809,7 +809,7 @@ def report_membership_query(user, **kwargs):
             dictbase["immat"] = head.chf_id
             dictbase["firstName"] = head.last_name
             dictbase["lastName"] = head.other_names
-            dictbase["address"] = head.current_address
+            dictbase["address"] = head.address
             if head.gender:
                 if head.gender.code == 'M':
                     dictbase["civiliteM"] = "x"
